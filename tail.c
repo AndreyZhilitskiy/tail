c86ec1c7dff88d196b4f9975869835308f058d14
Andrew Zhilitskiy <andrewzhilitskiy@gmail.com>
2017-04-24 13:47:05 +0300

    Initial commit

---
+++ tail.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define STDERR 2
+
+in main(int argc, char** argv)
+{
+	return 0;
+}
