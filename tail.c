036bb280896e6a3b18431e164263f7b6ca24b802
Andrew Zhilitskiy <andrewzhilitskiy@gmail.com>
2017-04-24 14:55:20 +0300

    adds usage

--- tail.c
+++ tail.c
@@ -9,7 +9,21 @@
 
 #define STDERR 2
 
-in main(int argc, char** argv)
+int usage(char* exec_name);
+
+int main(int argc, char** argv)
 {
+	
+	return 0;
+}
+
+int usage(char* exec_name)
+{
+	errno = EINVAL; /*invalid argument*/
+	char* s = "usage:\n";
+	char* s1 = " [file]";
+	write(STDERR, s, strlen(s));
+	write(STDERR, exec_name, strlen(exec_name));
+	write(STDERR, s1, strlen(s1));
 	return 0;
 }
